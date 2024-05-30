a88d15dd0bed0ab299aa8ffda8f965cfdf8536db
solonkonora <nkwadanora@gmail.com>
2024-05-30 14:38:08 +0100

    fixing deployment error encoutered

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ import logger from 'morgan';
 import YAML from 'yamljs';
 import swaggerUi from 'swagger-ui-express';
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
 import recipeRouter from './routes/recipes.js';
 import categoriesRouter from './routes/catergories.js';
 import ingredientsRouter from './routes/ingredients.js';
@@ -20,26 +18,25 @@ const swaggerDocument = YAML.load('./documentary/swagger-specs.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
-// app.use(logger('dev'));   //to loggin request in dev mode
+app.use(logger('dev'));   //to loggin request in dev mode
 app.use(json());
 app.use(express.urlencoded({ extended: true }));  //parse incoming json and url-encoded requst bodies
 app.use(cookieParser());
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-// app.use('/recipes', recipeRouter);
-// app.use('/categories', categoriesRouter);
-// app.use('/ingredients', ingredientsRouter);
-// app.use('/instructions', instructionsRouter);
-app.use('/api/recipes', recipeRouter);
-app.use('/api/categories', categoriesRouter);
-app.use('/api/ingredients', ingredientsRouter);
-app.use('/api/instructions', instructionsRouter);
+app.use('/recipes', recipeRouter);
+app.use('/categories', categoriesRouter);
+app.use('/ingredients', ingredientsRouter);
+app.use('/instructions', instructionsRouter);
+
+// app.use('/api/recipes', recipeRouter);
+// app.use('/api/categories', categoriesRouter);
+// app.use('/api/ingredients', ingredientsRouter);
+// app.use('/api/instructions', instructionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, 'route not found'));
 });
 
 
@@ -48,6 +45,9 @@ app.use(function(err, req, res, next) {
   // Set the response status to the error status or default to 500
   res.status(err.status || 500);
   
+   // Log the error
+   console.error(err);
+
   // Send a JSON response with the error message
   res.send({ message: err.message });
 });
