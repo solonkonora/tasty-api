a4e775c9a61706ea06163d2535e69b8a84032099
solonkonora <nkwadanora@gmail.com>
2024-06-05 19:37:09 +0100

    recent changes

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const router = express.Router();
+
+
+// Import the recipe-related and user-related routes
+import recipesRouter from './recipes.js';
+import usersRouter from './users.js';
+import categoriesRouter from './catergories.js';
+import ingredientRouter from './ingredients.js';
+import instructionsRouter from './instructions.js';
+
+
+// Mount the recipe-related and user-related routes
+router.use('/recipes', recipesRouter);
+router.use('/users', usersRouter);
+router.use('/categories', categoriesRouter);
+router.use('/ingredient', ingredientRouter);
+router.use('/instructions', instructionsRouter);
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {  // serves as home page for the app
+  res.render('index', { title: 'Express' });
+});
+
+export default router;
