4f07026af5aa70766a9b369ad0afdea87777aa8b
nkwadanora <nkwadanora@gmail.com>
2025-11-20 13:21:46 +0100

    chore: force deployment with version bump

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {  // serves as home page for the app
-  res.send({message: 'hello how  are you'})
+  res.send({message: 'hello how are you', version: '1.0.1'})
 });
 
 export default router;
